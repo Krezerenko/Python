906957926515eb803080387ccabe0ad42acb02f1
Krezon <krezony@gmail.com>
2025-03-14 10:35:51 +0300

    Классная работа

---
+++ class/ex5.1.py
@@ -0,0 +1,322 @@
+﻿inp = '''<div>
+  
+  <div>
+    <small class="mb-2 text-muted d-block">
+      <span>ИВБО</span>
+    </small>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/0">
+      ИВБО-10-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/1">
+      ИВБО-11-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/2">
+      ИВБО-12-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/3">
+      ИВБО-13-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/4">
+      ИВБО-20-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/5">
+      ИВБО-21-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/6">
+      ИВБО-22-23
+    </a>
+    
+  </div>
+  
+  <div>
+    <small class="mb-2 text-muted d-block">
+      <span>ИКБО</span>
+    </small>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/7">
+      ИКБО-10-22
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/8">
+      ИКБО-10-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/9">
+      ИКБО-11-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/10">
+      ИКБО-12-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/11">
+      ИКБО-13-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/12">
+      ИКБО-14-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/13">
+      ИКБО-15-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/14">
+      ИКБО-20-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/15">
+      ИКБО-21-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/16">
+      ИКБО-22-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/17">
+      ИКБО-24-22
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/18">
+      ИКБО-34-22
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/19">
+      ИКБО-40-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/20">
+      ИКБО-41-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/21">
+      ИКБО-42-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/22">
+      ИКБО-43-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/23">
+      ИКБО-50-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/24">
+      ИКБО-51-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/25">
+      ИКБО-52-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/26">
+      ИКБО-60-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/27">
+      ИКБО-61-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/28">
+      ИКБО-62-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/29">
+      ИКБО-63-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/30">
+      ИКБО-64-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/31">
+      ИКБО-65-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/32">
+      ИКБО-66-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/33">
+      ИКБО-67-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/34">
+      ИКБО-68-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/35">
+      ИКБО-70-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/36">
+      ИКБО-71-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/37">
+      ИКБО-72-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/38">
+      ИКБО-73-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/39">
+      ИКБО-74-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/40">
+      ИКБО-75-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/41">
+      ИКБО-76-23
+    </a>
+    
+  </div>
+  
+  <div>
+    <small class="mb-2 text-muted d-block">
+      <span>ИМБО</span>
+    </small>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/42">
+      ИМБО-10-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/43">
+      ИМБО-11-23
+    </a>
+    
+  </div>
+  
+  <div>
+    <small class="mb-2 text-muted d-block">
+      <span>ИНБО</span>
+    </small>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/44">
+      ИНБО-10-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/45">
+      ИНБО-11-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/46">
+      ИНБО-12-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/47">
+      ИНБО-13-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/48">
+      ИНБО-20-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/49">
+      ИНБО-21-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/50">
+      ИНБО-22-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/51">
+      ИНБО-23-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/52">
+      ИНБО-30-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/53">
+      ИНБО-31-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/54">
+      ИНБО-32-23
+    </a>
+    
+    <a class="btn btn-outline-primary mb-2 mr-3" href="/group/55">
+      ИНБО-33-23
+    </a>
+    
+  </div>
+  
+</div>'''
+
+ans = allGroups = '''ИВБО-10-23
+ИВБО-11-23
+ИВБО-12-23
+ИВБО-13-23
+ИВБО-20-23
+ИВБО-21-23
+ИВБО-22-23
+ИКБО-10-22
+ИКБО-10-23
+ИКБО-11-23
+ИКБО-12-23
+ИКБО-13-23
+ИКБО-14-23
+ИКБО-15-23
+ИКБО-20-23
+ИКБО-21-23
+ИКБО-22-23
+ИКБО-24-22
+ИКБО-34-22
+ИКБО-40-23
+ИКБО-41-23
+ИКБО-42-23
+ИКБО-43-23
+ИКБО-50-23
+ИКБО-51-23
+ИКБО-52-23
+ИКБО-60-23
+ИКБО-61-23
+ИКБО-62-23
+ИКБО-63-23
+ИКБО-64-23
+ИКБО-65-23
+ИКБО-66-23
+ИКБО-67-23
+ИКБО-68-23
+ИКБО-70-23
+ИКБО-71-23
+ИКБО-72-23
+ИКБО-73-23
+ИКБО-74-23
+ИКБО-75-23
+ИКБО-76-23
+ИМБО-10-23
+ИМБО-11-23
+ИНБО-10-23
+ИНБО-11-23
+ИНБО-12-23
+ИНБО-13-23
+ИНБО-20-23
+ИНБО-21-23
+ИНБО-22-23
+ИНБО-23-23
+ИНБО-30-23
+ИНБО-31-23
+ИНБО-32-23
+ИНБО-33-23'''
+
+def separate_groups(text):
+    return [g.replace("\n", "").replace(" ", "").removesuffix("</div></div>").removesuffix("</a>")[(i>=10) + 3:] for i, g in enumerate([x for g in text.split("<a class=\"btn btn-outline-primary mb-2 mr-3\" href=\"/group/") for x in g.split("</div>") if x.count("<small class=\"mb-2 text-muted d-block\">") == 0])][:-2]
+
+a = separate_groups(inp)
+print(a)
+print(ans.split("\n"))
+for i in range(len(a)):
+    assert(a[i] == ans.split("\n")[i])
+
